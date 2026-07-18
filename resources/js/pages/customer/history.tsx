1efe7071cc3108c35ae12d29127ca336b1c8fc9c
tuhu02 <230411100096@student.trunojoyo.ac.id>
2026-07-18 22:09:57 +0700

    update

--- resources/js/pages/customer/history.tsx
+++ resources/js/pages/customer/history.tsx
@@ -159,6 +159,7 @@ export default function History({
                             ))}
                         </div>
                     )}
+                </div>
             </div>
         </AppLayout>
     );
